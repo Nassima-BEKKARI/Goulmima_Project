7b8a50370b0cb2217737a3a24f395e17daa76d7a
nasalia <n.bekkari1987@gmail.com>
2022-07-19 10:01:39 +0200

    initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    public function home()
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
